e1ed0930de8c8702ebe6faa96f58b48bb145a7fb
Erick <edeajob27@gmail.com>
2023-02-04 15:46:06 -0500

    Corrigiendo error del cambio de titulo

--- playground/core/views.py
+++ playground/core/views.py
@@ -3,7 +3,15 @@ from django.views.generic.base import TemplateView
 
 # Usando CBV
 class HomePageView(TemplateView): # La clase tiene que heredar
-    template_name = "core/home.html" # Este atributo define cual es el template
+    template_name = "core/home.html"
+    context = {'title': 'Mi Super Web Playground'} # Este atributo define cual es el template
+#? Como modificar el diccionario de contexto con una CBV
+#* Se debe procesar la respuesta de la vista.
+#* La respuesta de la vista se define en el metodo get() si sobreescribimos get()
+#* Conseguiremos sobreescribir la respuesta en si misma
+#! Se le debe pasar self, request,*args,**kwargs los dos ultimos para buenas prácticas
 
+    def get(self, request):
+        return render(request,self.template_name,self.context)
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
