0ce1c03c1da4bcc0f2c846a4002653770dace42b
Erick <edeajob27@gmail.com>
2023-02-04 16:59:11 -0500

    Creando el page_menu

--- playground/pages/views.py
+++ playground/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Page
 
 # Create your views here.
@@ -9,3 +10,7 @@ class PageListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title','content','order']
