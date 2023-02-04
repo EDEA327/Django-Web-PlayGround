1011714b51428347f6002c9b61a81ddc1c51ab3a
Erick <edeajob27@gmail.com>
2023-02-04 17:36:52 -0500

    vista update

--- playground/pages/views.py
+++ playground/pages/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Page
 
 # Create your views here.
@@ -14,3 +14,17 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['title','content','order']
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title','content','order']
+    template_name_suffixed = '_update_form'
+    success_url = reverse_lazy('pages:pages')
+
+    def get_success_url(self):
+        return  reverse_lazy('pages:update',args = [self.object.id]) + '?ok'
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
