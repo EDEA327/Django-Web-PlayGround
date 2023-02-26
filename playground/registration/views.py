2a7fccea4a700432724388b99e8bb9aa10df05e1
Erick <edeajob27@gmail.com>
2023-02-26 11:58:22 -0500

    Fin de la creación del modelo perfil

--- playground/registration/views.py
+++ playground/registration/views.py
@@ -1,5 +1,8 @@
 from .forms import UserCretionFormwithEmail
 from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
 
@@ -25,3 +28,9 @@ class SignUpView(CreateView):
                                                                     'placeholder':'Repite la contraseña'})
 
         return form
+
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(TemplateView):
+    template_name = 'registration/profile_form.html'
