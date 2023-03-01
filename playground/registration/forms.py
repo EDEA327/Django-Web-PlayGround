f066bbe9d6fbd3dbd5275e839ba6351944d6d915
Erick <edeajob27@gmail.com>
2023-03-01 17:48:38 -0500

    Creacion vista de EmailUpdate

--- playground/registration/forms.py
+++ playground/registration/forms.py
@@ -27,4 +27,23 @@ class ProfileForm(forms.ModelForm):
 
         }
 
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required = True, help_text="Requerido,254 caracteres como maximo y deber ser válido.")
+
+    class Meta:
+        model = User #Ya tiene sus validaciones internas
+        fields = ['email']
+        #! la unica forma de poner widgets sin cargarte las validaciones de User
+        #! Es poniendo los widgets en tiempo de ejecución osea en la vista.
+        #! Ojo con la validacion del correo que no debe existir antes en la base datos.
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        #* el primer if es para saber si el email cambió
+        if 'email' in self.changed_data:
+            #* Pra saber si el nuevo email ya existía.
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado pruebe con otro por favor")
+        return email
+
+
 
