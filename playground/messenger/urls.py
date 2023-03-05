d3e408a11d254f13eef941e23acb243bb89b5a9f
Erick <edeajob27@gmail.com>
2023-03-05 08:34:24 -0500

    mejorando el hilo con JavaScript

--- playground/messenger/urls.py
+++ playground/messenger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ThreadListView,ThreadDetailView
+from .views import ThreadListView,ThreadDetailView,add_message
 
 messenger_patterns = ([
     path('',ThreadListView.as_view(),name='list'),
     path('thread/<int:pk>/',ThreadDetailView.as_view(),name='detail'),
+    path('thread/<int:pk>/add/',add_message,name='add'),
 
 ],"messenger")
--- playground/messenger/views.py
+++ playground/messenger/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import render
+from django.http import Http404,JsonResponse
+from django.shortcuts import render,get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 
-from .models import Thread
+from .models import Thread,Message
 
 
 # Create your views here.
@@ -21,3 +21,19 @@ class ThreadDetailView(DetailView):
         obj = super(ThreadDetailView, self).get_object()
         if self.request.user not in obj.users.all():
             raise  Http404()
+
+def add_message(request,pk):
+    json_response = {
+        'created':False,
+    }
+    if request.user.is_authenticated():
+        content = request.GET.get('content',None)
+        if content:
+            thread = get_object_or_404(Thread,pk=pk)
+            message = Message.objects.create(user=request.user,content =content)
+            thread.messages.add(message)
+            json_response["created"] = True
+
+    else:
+        raise Http404("User is not authenticated")
+    return JsonResponse(json_response)
